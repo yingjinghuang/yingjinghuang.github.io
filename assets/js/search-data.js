8ffc0bf0a90c95c9c46363444c0cd5f93f2874d7
yingjinghuang <42825492+yingjinghuang@users.noreply.github.com>
2026-04-09 21:56:02 +0000

    Deploying to gh-pages from @ yingjinghuang/yingjinghuang.github.io@e12b568782c9eec54a2cbe68ac7741e3b8f336be 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -86,6 +86,9 @@ ninja.data = [{
           section: "News",},{id: "news-i-started-my-position-as-a-university-assistant-postdoc-at-the-university-of-vienna",
           title: 'I started my position as a University Assistant Postdoc at the University of...',
           description: "",
+          section: "News",},{id: "news-call-for-papers-we-are-thrilled-to-share-the-call-for-papers-for-the-geography-according-to-foundation-models-workshop-at-agile-2026-we-welcome-short-papers-4-6-pages-on-late-breaking-results-conceptual-papers-and-experiments-describing-how-ai-represents-and-potentially-shapes-geography-submission-deadline-april-29-2026-workshop-date-june-16-2026-tartu-estonia",
+          title: '📢 Call for Papers! We are thrilled to share the Call for Papers...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
